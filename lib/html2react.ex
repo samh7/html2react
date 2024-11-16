646515c8b65dc18c3df770a60c75baac440d0eb8
samh7 <samh74490@example.com>
2024-11-16 20:55:31 +0300

    add html to react parser

--- lib/html2react.ex
+++ lib/html2react.ex
@@ -4,15 +4,39 @@ defmodule Html2react do
   """
 
   @doc """
-  Hello world.
+  Parse HTML to React.
 
   ## Examples
 
-      iex> Html2react.hello()
-      :world
+      iex> Html2react.parse("<div class='test'>Hello</div>")
+      "<div className='test'>Hello</div>"
 
   """
-  def hello do
-    :world
+  def parse(html), do: get_tags(html)
+
+  defp get_tags([{"html" = tag, attributes, children}]) do
+    "<#{tag} #{get_attributes(attributes)}> #{get_tags(children)} </#{tag}>"
+  end
+
+  defp get_tags([{tag, attributes, children} | rest]) do
+    "<#{tag} #{get_attributes(attributes)}> #{get_tags(children ++ rest)} </#{tag}>"
+  end
+
+  defp get_tags([text | rest]), do: text <> get_tags(rest)
+  defp get_tags([]), do: ""
+
+  defp get_attributes(list) do
+    list
+    |> Enum.reduce("", fn {k, v}, a ->
+      "#{a} #{convert_attribute_key(k)}=#{<<34>>}#{v}#{<<34>>} "
+    end)
+  end
+
+  defp convert_attribute_key(attr) do
+    case attr do
+      "class" -> "className"
+      "for" -> "htmlFor"
+      _ -> attr
+    end
   end
 end
